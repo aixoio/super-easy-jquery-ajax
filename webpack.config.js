0c2aa0abce7f30331461093d23ee3c94820945e2
aixoio <79969530+aixoio@users.noreply.github.com>
2023-03-02 19:06:22 +1100

    Add web build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path")
+
+module.exports = {
+
+    mode: "production",
+
+    entry: {
+
+        bundle: path.resolve(__dirname, "src/index.js")
+
+    },
+
+    output: {
+        
+        path: path.resolve(__dirname, "dist"),
+        filename: "super-easy-jquery-ajax.web.js"
+
+    }
+
+}
